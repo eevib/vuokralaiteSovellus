acd8fc4edb87f374f7742f2e93415680e5892ae5
Eevi Bengs <eevi.bengs@helsinki.fi>
2021-05-29 10:31:24 +0300

    Add customer

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app=Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL") #"postgresql:///eebe" #
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
@@ -22,8 +22,7 @@ def login():
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if user == None:
-        print("Virheellinen käyttäjätunnus, yritä uudelleen")
-        return redirect("/")
+        return render_template("error.html", message="Virheellinen käyttjätunnus tai salasana")
     else: 
         hash_value = user[0]
         if check_password_hash(hash_value,password):
@@ -45,7 +44,7 @@ def register():
     username = request.form["username"]
     password = request.form["password"]
     if len(password) < 3 or len(username) < 3: 
-        print ("Käyttäjätunnus ja salasana on oltava vähintään 3 merkkiä pitkä.")
+         return render_template("error.html", message="Käyttäjätunnus ja salasana on oltava vähintään 3 merkkiä pitkä.")
     else: 
         hash_value = generate_password_hash(password)
         sql = "INSERT INTO users (username,password) VALUES (:username, :password)"
@@ -56,3 +55,16 @@ def register():
 @app.route("/mainPage")
 def mainPage():
     return render_template("mainPage.html")
+
+@app.route("/customer")
+def customer():
+    return render_template("customer.html")
+
+@app.route("/addCustomer", methods=["POST"])
+def addCustomer():
+    name = request.form["name"]
+    email = request.form["email"]
+    sql = "INSERT INTO customers (name,email) VALUES (:name, :email)"
+    db.session.execute(sql, {"name":name,"email":email})
+    db.session.commit()
+    return redirect("/mainPage")
